fb2e5f74144bd7f126247152e123e351b2d3fba6
jassa3206 <jsingh77247@gmail.com>
2024-11-08 20:32:16 +0530

    doing some SEO  shit don't know if its gonna work  and also worked on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import { useAppContext } from "@/contexts/AppContext"
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+// import {  signOut } from "next-auth/react";
 import { useEffect } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { GiCargoShip } from "react-icons/gi";
 
 const Header: React.FC = () => {
     const { data: session } = useSession();
@@ -23,31 +26,48 @@ const Header: React.FC = () => {
     }, [session, setUser, setIsAuthenticated])
 
     return (
-        <header className="bg-transparent py-4 px-8 flex justify-between items-center">
-            <div className="text-white text-2xl font-semibold">
-                <span style={{ textShadow: "0px 0px 10px rgba(0,0,0,0.8)" }}>FileFerry</span>
+        <header className="bg-transparent py-4 px-4 flex justify-between items-center">
+            <div className="flex items-center"  style={{ textShadow: "0px 0px 10px rgba(0,0,0,0.8)" }}>
+                <span className="p-1 text-white text-2xl font-semibold" >FileFerry</span>
+                <GiCargoShip color="white" size={24} className=""/>
             </div>
-            <div className="flex items-center space-x-4">
+            <div className="flex bg-white px-1 py-1 rounded-md items-center ">
                 {isAuthenticated ? (
                     <>
-                        <span className="text-white">Welcome, {user?.name}</span>
-                        <img src={user?.image} alt="Profile" className="w-8 h-8 rounded-full" />
-                        <button
+                        <span className="text-slate-950 ps-4 text-sm">Hey, {user?.name}</span>
+                        <img src={user?.image} alt="Profile" className="w-8 h-8 ms-1 rounded-md border-[0.5px] border-solid border-slate-950 " />
+                        <div>
+                            <BsThreeDotsVertical size={20} className="text-slate-950 ps-1 m-0" />
+                        </div>
+                        {/* <button
                             type="button"
                             onClick={() => signOut()}
                             className="text-white bg-red-500 px-4 py-2 rounded-md hover:bg-red-700 transition"
                         >
                             Logout
-                        </button>
+                        </button> */}
                     </>
                 ) : (
-                    <button
-                        type="button"
-                        onClick={() => signIn("google")}
-                        className="text-white bg-blue-500 px-4 py-2 rounded-md hover:bg-blue-700 transition"
-                    >
-                        Login
-                    </button>
+                    <div className="flex flex-row py-1">
+                        <div className="sign-in ">
+                            <button
+                                type="button"
+                                onClick={() => signIn("google")}
+                                className="text-slate-950 bg-transparent px-2 text-sm py-1 rounded-md"
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                        <div className="log-in">
+                            <button
+                                type="button"
+                                onClick={() => signIn("google")}
+                                className="text-white bg-slate-950 px-3 py-1 rounded-md text-sm hover:bg-slate-700 transition"
+                            >
+                                Log In
+                            </button>
+                        </div>
+                    </div>
                 )}
             </div>
         </header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,24 @@ import { fetchDailyBackgrounds } from "@/utils/fetchDailyBackground";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import { SessionProvider } from 'next-auth/react'
+import Head from "next/head";
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [background, setBackground] = useState<string>("")
+
+  //   useEffect(() => {
+  //     const fetchBackgroundImage = async () => {
+  //         const background = await fetchDailyBackgrounds();
+  //         console.log(background);
+  //         setBackground(background);
+  //     };
+
+  //     fetchBackgroundImage();
+
+  //     const intervalId = setInterval(fetchBackgroundImage, 120000);
+
+  //     return () => clearInterval(intervalId);
+  // }, []);
 
   useEffect(() => {
     const fetchBackgroundImage = async () => {
@@ -21,6 +36,10 @@ export default function App({ Component, pageProps: {session, ...pageProps} }: A
   return (
     <SessionProvider session={session}>
       <AppProvider>
+        <Head>
+          <title>FileFerry</title>
+          <meta name="description" content="file ferry  is file  transfer platform. you can transfer files freely using emails to any point of the earth and these files will stay there for which ever time you wants." />
+        </Head>
         <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${background})` }}>
           <Header />
           <main>
