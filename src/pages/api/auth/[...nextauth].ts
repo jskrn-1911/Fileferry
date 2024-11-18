c12422fce9853fe69fd02fa60c7f1da680f09cf5
jassa3206 <jsingh77247@gmail.com>
2024-11-18 16:33:15 +0530

    ok

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import connectToDatabase from "@/utils/mongoDb"
-import User from "@/models/User";
+import User from "@/models/User"
 
 export default NextAuth({
     providers: [
