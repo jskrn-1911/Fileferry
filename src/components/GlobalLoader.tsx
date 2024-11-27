cfe6cdf36922f76ffe66c9206fe8466a326d2e07
jassa3206 <jsingh77247@gmail.com>
2024-11-27 21:26:31 +0530

    added react-dropzone, file adding, file removing, steps, updated loader and many more

--- src/components/GlobalLoader.tsx
+++ src/components/GlobalLoader.tsx
@@ -1,11 +1,19 @@
-// src/components/GlobalLoader.tsx
+import React from "react";
+
 const GlobalLoader: React.FC = () => {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-transparent z-50">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-slate-950 border-solid"></div>
+  return (
+    <>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        {/* <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-white border-solid"></div> */}
+      <div className="loading-wave">
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
       </div>
-    );
-  };
-  
-  export default GlobalLoader;
-  
+      </div>
+    </>
+  );
+};
+
+export default GlobalLoader;
